3b842c7bac4cb1519c3eec9979417d67e739e894
LOWERTOP <onone.so@gmail.com>
2026-09-12 22:41:58 +0800

    Update build-modules.js

--- scripts/build-modules.js
+++ scripts/build-modules.js
@@ -18,6 +18,8 @@ const CONFIG = {
 };
 
 const APP_ALIASES = {
+    "🅳🅴🆅🆃🅾🅾🅻": ["🅲🅴🆁🆃🅷🆄🅱 证书模块"], "Plugin2Rocket": ["🅲🅴🆁🆃🅷🆄🅱 证书模块"],
+    "一汽大众": ["fawvw"], "上汽大众": ["csvw"], "流媒体": ["netflix"], "影视": ["netflix"], "大师兄": ["netflix"],
     "苹果": ["apple"], "apple": ["apple"], "谷歌": ["google"], "微软": ["microsoft"],
     "油管": ["youtube"], "youtube": ["youtube"], "电报": ["telegram"], "推特": ["twitter", "x"],
     "奈飞": ["netflix"], "网飞": ["netflix"], "迪士尼": ["disney"], "cmcc": ["中国移动"],
@@ -30,8 +32,7 @@ const APP_ALIASES = {
     "腾讯": ["tencent"], "美团": ["meituan"], "拼多多": ["pdd", "pinduoduo"],
     "闲鱼": ["xianyu"], "咸鱼": ["xianyu"], "饿了么": ["eleme"], "爱奇艺": ["iqiyi"],
     "优酷": ["youku"], "淘宝": ["taobao"], "豆瓣": ["douban"], "贴吧": ["tieba"],
-    "夸克": ["quark"], "12306": ["12306"], "高德地图": ["amap"],
-    "一汽大众": ["fawvw"], "上汽大众": ["csvw"], "流媒体": ["netflix"], "影视": ["netflix"], "大师兄": ["netflix"], "🅳🅴🆅🆃🅾🅾🅻": ["🅲🅴🆁🆃🅷🆄🅱 证书模块"], "Plugin2Rocket": ["🅲🅴🆁🆃🅷🆄🅱 证书模块"]
+    "夸克": ["quark"], "12306": ["12306"], "高德地图": ["amap"]
 };
 
 const FLAG_CODES = new Set([
